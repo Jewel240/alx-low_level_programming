6582a4187e0a86739051a851a0babab5935b8635
Jewel240 <ganiuzainab1608@gmail.com>
2023-09-09 20:56:54 -0700

    4-print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,12 @@ int main(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
 
-for (letter = 'A'; letter <= 'Z'; letter++)
+{
+if (letter != 'e' && letter != 'q')
 putchar(letter);
+}
 putchar('\n');
+
 return (0);
 }
